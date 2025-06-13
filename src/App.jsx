dd585e50df649c2a5a0eef3fbcc02a6402905365
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2025-06-13 16:52:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import AppRoutes from './routes/AppRoutes';
-
+import './App.css';
 const App = () => {
   const [user, setUser] = useState(() => {
     try {
